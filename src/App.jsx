e2926e5cc018d8982942f6d674a68bf72b6d312e
AndrewPerepelkin <avp.tep@rambler.ru>
2023-01-17 15:04:40 +0300

    feat: added tailwind css library

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div className="min-h-screen bg-slate-50 dark:bg-slate-900 transition-colors duration-150 flex flex-col">
      <h1>Money Stash App</h1>
     </div>
   );
